a9c37660e9c6a783d227f2ae76c4c0aa3bdc6ea7
Maheshdevops20 <maheshreddyyvr@gmail.com>
2026-02-22 21:30:39 +0530

    free memory added

--- bash_scripts/memory_info.sh
+++ bash_scripts/memory_info.sh
@@ -5,5 +5,6 @@ memory_info(){
     mem_info=$(free -m)
     total_mem=$(echo "$mem_info" | awk  '/Mem:/ {print $2}')
     currently_utillized_mem=$(echo "$mem_info" | awk '/Mem:/ {print $3}')
+    free_memory=$(echo "$mem_info" | awk '/Mem:/ {print $4}')
     echo "Total_memory: $total_mem MB, Used_memory: $currently_utillized_mem MB"
 }
