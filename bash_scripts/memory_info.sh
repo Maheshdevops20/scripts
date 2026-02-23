9a843f5359977257c1a0ce4047fbc2f4e427dc8e
Maheshdevops20 <maheshreddyyvr@gmail.com>
2026-02-23 22:54:41 +0530

    just added space

--- bash_scripts/memory_info.sh
+++ bash_scripts/memory_info.sh
@@ -6,5 +6,5 @@ memory_info(){
     total_mem=$(echo "$mem_info" | awk  '/Mem:/ {print $2}')
     currently_utillized_mem=$(echo "$mem_info" | awk '/Mem:/ {print $3}')
     free_memory=$(echo "$mem_info" | awk '/Mem:/ {print $4}')
-    echo "Total_memory: $total_mem MB, Used_memory: $currently_utillized_mem MB, Free_memory: $free_memory MB"
+    echo "Total_memory: $total_mem MB, Used_memory: $currently_utillized_mem MB, Free_memory: $free_memory MB  "
 }
